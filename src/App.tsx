f48698cb9bc2f3bb5187c7085258ea8526c689ce
callumfairbairn <callum_fairbairn@hotmail.com>
2024-06-01 13:02:40 +0100

    Add input and display letters in a circle

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,48 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import './App.css'
-import { Button } from './components/ui/button'
+import { Input } from './components/ui/input'
+
+const BASE_RADIUS = 100
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState('');
+  const randomisedInput = input.split('').sort(() => Math.random() < 0.5 ? -1 : 1)
+  const radius = getRadius(input.length)
 
   return (
-    <>
-      <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count  {count}
-        </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+    <div className='flex flex-col items-center gap-2'>
+      <h4 className='scroll-m-20 text-xl font-semibold tracking-tight'>Start typing</h4>
+      <Input className='max-w-sm' onChange={(e) => { setInput(e.target.value.toLocaleUpperCase()) }} />
+      <div style={{ paddingTop: radius, marginTop: '1.5rem' }}>
+        {randomisedInput.map((char, i) =>
+          <div
+            key={i}
+            className={`absolute border h-7 w-7 text-xl`}
+            style={{ ...getTransform(input.length, i, radius) }}
+          >
+            {char}
+          </div>
+        )}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
+}
+
+const getRadius = (inputLength: number) => {
+  const charLimit = 13
+  if (inputLength > charLimit) {
+    return BASE_RADIUS
+  }
+
+  return (BASE_RADIUS / charLimit) * inputLength
+}
+
+const getTransform = (inputLength: number, index: number, radius: number) => {
+  const yTranslate = Math.floor(Math.cos((index / inputLength) * 2 * Math.PI) * radius)
+  const xTranslate = Math.floor(Math.sin((index / inputLength) * 2 * Math.PI) * radius)
+  return {
+    transform: `translate(${xTranslate}px, ${-yTranslate}px)`
+  }
 }
 
 export default App
