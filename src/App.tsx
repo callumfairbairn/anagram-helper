777a9725cec970e7388b85f54977b118af3bc738
callumfairbairn <callum_fairbairn@hotmail.com>
2024-06-01 18:15:30 +0100

    Add shuffle button

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,41 @@
 import { useState } from 'react';
 import './App.css'
 import { Input } from './components/ui/input'
+import { Button } from './components/ui/button';
+import { ShuffleIcon } from 'lucide-react';
 
 const BASE_RADIUS = 100
+const boxDim = 27
 
 function App() {
-  const [input, setInput] = useState('');
-  const randomisedInput = input.split('').sort(() => Math.random() < 0.5 ? -1 : 1)
+  const [input, setInput] = useState<string[]>([]);
   const radius = getRadius(input.length)
 
   return (
-    <div className='flex flex-col items-center gap-2'>
-      <h4 className='scroll-m-20 text-xl font-semibold tracking-tight'>Start typing</h4>
-      <Input className='max-w-sm' onChange={(e) => { setInput(e.target.value.toLocaleUpperCase()) }} />
-      <div style={{ paddingTop: radius, marginTop: '1.5rem' }}>
-        {randomisedInput.map((char, i) =>
-          <div
-            key={i}
-            className={`absolute border h-7 w-7 text-xl`}
-            style={{ ...getTransform(input.length, i, radius) }}
-          >
-            {char}
-          </div>
-        )}
+    <div className='flex flex-col items-center'>
+      <div className='flex flex-col items-center gap-3'>
+        <h4 className='scroll-m-20 text-xl font-semibold tracking-tight w-72'>Start typing</h4>
+        <Input className='max-w-sm' onChange={(e) => { setInput(e.target.value.toLocaleUpperCase().split('').sort(randomSort)) }} />
+        <div className='self-end shrink-3'>
+          <Button onClick={() => setInput(prevValue => [...prevValue.sort(randomSort)])} className='self-end'><ShuffleIcon /></Button>
+        </div>
+        <div style={{ paddingTop: radius }}>
+          {input.map((char, i) =>
+            <div
+              key={`${i}-${char}`}
+              className={`absolute border text-xl`}
+              style={{ width: `${boxDim}px`, height: `${boxDim}px`, ...getTransform(input.length, i, radius) }}
+            >
+              {char}
+            </div>
+          )}
+        </div>
       </div>
     </div>
   )
 }
+
+const randomSort = () => Math.random() < 0.5 ? -1 : 1
 
 const getRadius = (inputLength: number) => {
   const charLimit = 13
@@ -41,7 +50,7 @@ const getTransform = (inputLength: number, index: number, radius: number) => {
   const yTranslate = Math.floor(Math.cos((index / inputLength) * 2 * Math.PI) * radius)
   const xTranslate = Math.floor(Math.sin((index / inputLength) * 2 * Math.PI) * radius)
   return {
-    transform: `translate(${xTranslate}px, ${-yTranslate}px)`
+    transform: `translate(${xTranslate - boxDim / 2}px, ${-yTranslate}px)`
   }
 }
 
