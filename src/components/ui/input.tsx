a25305c28d0b23e45845204ec5be1b92c68d3ec8
callumfairbairn <callum_fairbairn@hotmail.com>
2024-06-04 20:37:20 +0100

    Remove padding from undelined inputs

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -39,7 +39,7 @@ const UnderlineInput = React.forwardRef<HTMLInputElement, InputProps>(
         onChange={handleChange}
         type={type}
         className={cn(
-          "flex h-10 w-full border-b-2 uppercase border-slate-900 bg-white px-3 py-2 text-lg ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring- focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300",
+          "flex text-center h-10 w-full border-b-2 uppercase border-slate-900 bg-white py-2 text-lg ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring- focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300",
           className
         )}
         ref={ref}
